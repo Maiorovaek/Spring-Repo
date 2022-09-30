ffd50775b67c01c1b2292703ff9e292e13b0ba38
Maiorovaek <katushkm@yandex.ru>
2022-09-30 11:33:48 +0300

    Init project

---
+++ SensorMeasurementsApp/src/main/java/com/example/SensorMeasurementsApp/SensorMeasurementsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.SensorMeasurementsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorMeasurementsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorMeasurementsAppApplication.class, args);
+	}
+
+}
---
+++ SensorMeasurementsApp/src/test/java/com/example/SensorMeasurementsApp/SensorMeasurementsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SensorMeasurementsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorMeasurementsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
